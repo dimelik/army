9cee87fd6fd65806ae0bc9b6f6c83d6ff7dc079d
dmitry <dmitry@example.ru>
2020-07-12 14:39:16 +0300

    set map for army

--- army/army.py
+++ army/army.py
@@ -12,14 +12,12 @@ class Army:
             self.__soldiers_keys.append(soldier.get_soldier_key())
 
     def get_soldiers(self):
-        result = []
         for soldier_key in self.__soldiers_keys:
-            result.append(self.__map.get(soldier_key))
-        return result
+            yield self.__map.get(soldier_key)
 
     def army_weapon_price(self):
-        result = self.get_soldiers()[0].get_weapon_price()
-        for key in self.get_soldiers():
-            if key != self.get_soldiers()[0]:
-                result = result.add(key.get_weapon_price())
+        gen_soldiers = self.get_soldiers()
+        result = next(gen_soldiers).get_weapon_price()
+        for key in gen_soldiers:
+            result = result.add(key.get_weapon_price())
         return result
